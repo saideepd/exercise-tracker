5f7c4575a9c8366a8084e6d4383ad5fa6853dfb7
Saideep Dicholkar <dicholkarsaideep@gmail.com>
2022-02-03 00:10:46 +0530

    Added Mongo DB connection

--- server/index.js
+++ server/index.js
@@ -1,5 +1,8 @@
 const express = require('express');
+require('dotenv').config();
 const cors = require('cors');
+const bodyParser = require('body-parser');
+const mongoose = require('mongoose');
 
 const PORT = process.env.PORT || 3001;
 
@@ -7,14 +10,25 @@ const app = express();
 
 app.listen();
 
-// enable CORS
+// Enable CORS
 app.use(cors({ optionsSuccessStatus: 200 }));
+
+// Adding body-parser middleware
+app.use(bodyParser.urlencoded({ extended: false }));
 
 // Hide X-powered-by header ;)
 app.use(function (req, res, next) {
     res.header("X-powered-by", "Efforts, Sweat, Dedication and Desire");
     next();
 });
+
+// Connect to Mongo DB
+let connection = mongoose.connect
+    (process.env.MONGO_URI, {
+        useNewUrlParser: true,
+        useUnifiedTopology: true
+    });
+
 
 app.get("/api", (req, res) => {
     res.json({ message: "Hellp from Express!" });
