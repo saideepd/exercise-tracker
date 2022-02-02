b21d2e2efe292c0a467acb5b3050c691c8cf3ffb
Saideep Dicholkar <dicholkarsaideep@gmail.com>
2022-02-03 02:22:46 +0530

    Added endpoint to create user with basic error handling

--- server/functions/user.js
+++ server/functions/user.js
@@ -5,7 +5,32 @@ const userSchema = new mongoose.Schema({
     username: { type: String, unique: true, required: true }
 });
 
-
 const User = mongoose.model('User', userSchema);
 
+const createUser = (input, done) => {
+    if (input === undefined || input === null) {
+        console.log(`Invalid input for username ${input}`);
+        done(null, { error: "Invalid input" });
+        return { error: "Invalid input" };
+    }
+    
+    let userObject = User({ username: input });
+    console.log(userObject);
+
+    User.create(userObject, (createUserError, createdUser) => {
+        if (createUserError) {
+            // console.log(createUserError);
+            createUserError.message = createUserError.toString().includes("duplicate") ?
+                                        "User already exists" : "Error creating user";
+            done(null, createUserError);
+            return { error: createUserError };
+        }
+        createdUser.message = "User created successfully";
+        done(null, createdUser);
+        return createdUser;
+    });
+};
+
+
 exports.UserModel = User;
+exports.createUser = createUser;
--- server/index.js
+++ server/index.js
@@ -3,6 +3,9 @@ require('dotenv').config();
 const cors = require('cors');
 const bodyParser = require('body-parser');
 const mongoose = require('mongoose');
+const serverless = require('serverless-http');
+const router = express.Router();
+const user = require('./functions/user');
 
 const PORT = process.env.PORT || 3001;
 
@@ -30,16 +33,54 @@ let connection = mongoose.connect
     });
 
 
-app.get("/api", (req, res) => {
-    res.json({ message: "Hellp from Express!" });
+router.get("/api", (req, res) => {
+    res.json({ message: "Hello from Express!" });
 });
 
-app.get("/api/hello", (req, res) => {
+router.get("/api/hello", (req, res) => {
     res.json({ message: "Hello, World!" });
 });
+
+router.post("/api/users", (req, res, next) => {
+    let username = req.body.username;
+    console.log(`User Input: ${username}`);
+
+    if (!username || username === undefined || typeof username === undefined || username === '') {
+        console.log(`Invalid or blank username input`);
+        res.json({ error: "Invalid username input" });
+        return next({ error: "Invalid username input" });
+    }
+    
+    user.createUser(username, (err, responseData) => {
+        if (err) {
+            console.log(`Error creating user: ${err}`);
+            res.status(400).json({ error: err });
+            return next(err);
+        }
+        else if (!responseData) {
+            console.log('Missing `done()` argument');
+            res.status(500).json({ error: "Error creating record" });
+            return next({ error: "Error creating record" });
+        }
+        else if (responseData.toString().toLowerCase().includes('error' || 'exists' || 'already')) {
+            console.log(`Error in create user response data: ${JSON.stringify(responseData)}`);
+            res.status(422).json({ error: "Username already taken. Please try again with another username." });
+        }
+        else if (Object.entries.length == 0) {
+            res.status(422).json({ error: "Error creating user" });
+        }
+        else {
+            console.log(`Create User Response Data: ${responseData}`);
+            res.status(201).json({ _id: responseData.id, username: responseData.username });
+        }
+    });
+});
+
+
+app.use('/', router);
 
 app.listen(PORT, () => {
     console.log(`Server is listening on ${PORT}`);
 });
 
-module.exports = app;
+module.exports.handler = serverless(app);
