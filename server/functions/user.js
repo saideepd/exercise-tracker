7eb2b8ba577a5cad1f5ed71bcabe845c3d0b2bbe
Saideep Dicholkar <dicholkarsaideep@gmail.com>
2022-02-03 22:54:02 +0530

    Added Get User by Id & Username API endpoints

--- server/functions/user.js
+++ server/functions/user.js
@@ -2,7 +2,11 @@ const mongoose = require('mongoose');
 
 
 const userSchema = new mongoose.Schema({
-    username: { type: String, unique: true, required: true }
+    username: {
+        type: String,
+        unique: true,
+        required: true
+    }
 });
 
 const User = mongoose.model('User', userSchema);
@@ -10,20 +14,28 @@ const User = mongoose.model('User', userSchema);
 const createUser = (input, done) => {
     if (input === undefined || input === null) {
         console.log(`Invalid input for username ${input}`);
-        done(null, { error: "Invalid input" });
-        return { error: "Invalid input" };
+        done(null, {
+            error: "Invalid input"
+        });
+        return {
+            error: "Invalid input"
+        };
     }
-    
-    let userObject = User({ username: input });
+
+    let userObject = User({
+        username: input
+    });
     console.log(userObject);
 
     User.create(userObject, (createUserError, createdUser) => {
         if (createUserError) {
             // console.log(createUserError);
             createUserError.message = createUserError.toString().includes("duplicate") ?
-                                        "User already exists" : "Error creating user";
+                "User already exists" : "Error creating user";
             done(null, createUserError);
-            return { error: createUserError };
+            return {
+                error: createUserError
+            };
         }
         createdUser.message = "User created successfully";
         done(null, createdUser);
@@ -31,6 +43,44 @@ const createUser = (input, done) => {
     });
 };
 
+const findUserById = (input, done) => {
+    console.log(`findUserById Input: ${input}`);
+    User.findById(input, (findUserError, userFound) => {
+        if (findUserError) {
+            console.log(`Error finding user by id: ${findUserError}`);
+            done(null, {
+                error: "Error finding user by id"
+            });
+            return (`Error finding user by id: ${findUserError}`);
+        }
+        console.log(`Result findUserById: ${userFound}`);
+        done(null, userFound);
+        return userFound;
+    });
+};
+
+const findUserByUsername = (input, done) => {
+    console.log(`findUserByUsername Input: ${input}`);
+    User.findOne({
+        username: {
+            $eq: input
+        }
+    }, (findUserError, userFound) => {
+        if (findUserError) {
+            console.log(`Error finding user by username: ${findUserError}`);
+            done(null, {
+                error: "Error finding user by username"
+            });
+            return (`Error finding user by username: ${findUserError}`);
+        }
+        console.log(`findUserByUsername: ${userFound}`);
+        done(null, userFound);
+        return userFound;
+    });
+};
+
 
 exports.UserModel = User;
 exports.createUser = createUser;
+exports.findUserById = findUserById;
+exports.findUserByUsername = findUserByUsername;
--- server/index.js
+++ server/index.js
@@ -32,25 +32,28 @@ let connection = mongoose.connect
         useUnifiedTopology: true
     });
 
-
+// Default API Endpoint
 router.get("/api", (req, res) => {
-    res.json({ message: "Hello from Express!" });
+    res.json({ message: "Hello from Exercise Tracker!" });
 });
 
+// Test Hello Endpoint
 router.get("/api/hello", (req, res) => {
     res.json({ message: "Hello, World!" });
 });
 
+// Create User API Endpoint
 router.post("/api/users", (req, res, next) => {
     let username = req.body.username;
     console.log(`User Input: ${username}`);
 
+    // Error handling for invalid input
     if (!username || username === undefined || typeof username === undefined || username === '') {
         console.log(`Invalid or blank username input`);
         res.json({ error: "Invalid username input" });
         return next({ error: "Invalid username input" });
     }
-    
+
     user.createUser(username, (err, responseData) => {
         if (err) {
             console.log(`Error creating user: ${err}`);
@@ -72,6 +75,80 @@ router.post("/api/users", (req, res, next) => {
         else {
             console.log(`Create User Response Data: ${responseData}`);
             res.status(201).json({ _id: responseData.id, username: responseData.username });
+        }
+    });
+});
+
+// Find User By Id API Endpoint
+router.get("/api/users/:_id", (req, res, next) => {
+    let userId = req.params._id;
+    console.log(`User Input: ${userId}`);
+
+    // Error handling for invalid input
+    if (!userId || userId === undefined || typeof userId === undefined || userId === '') {
+        console.log(`Invalid or blank userId input`);
+        res.json({ error: "Invalid userId input" });
+        return next({ error: "Invalid userId input" });
+    }
+
+    user.findUserById(userId, (err, responseData) => {
+        if (err) {
+            console.log(`Error finding user: ${err}`);
+            res.status(400).json({ error: err });
+            return next(err);
+        }
+        else if (!responseData) {
+            console.log('Missing `done()` argument');
+            res.status(400).json({ error: "Error finding user record" });
+            return next({ error: "Error finding user record" });
+        }
+        else if (responseData.toString().toLowerCase().includes('error' || 'exists' || 'already')) {
+            console.log(`Error in findUserById response data: ${JSON.stringify(responseData)}`);
+            res.status(400).json({ error: "User does not exists with provided id. Please try again with correct user id." });
+        }
+        else if (Object.entries.length == 0) {
+            res.status(400).json({ error: "Error finding user" });
+        }
+        else {
+            console.log(`Find User By Id Response Data: ${responseData}`);
+            res.status(200).json({ _id: responseData.id, username: responseData.username });
+        }
+    });
+});
+
+// Find User By Username API Endpoint
+router.get("/api/username/:username", (req, res, next) => {
+    let userName = req.params.username;
+    console.log(`User Input: ${userName}`);
+
+    // Error handling for invalid input
+    if (!userName || userName === undefined || typeof userName === undefined || userName === '') {
+        console.log(`Invalid or blank username input`);
+        res.json({ error: "Invalid username input" });
+        return next({ error: "Invalid username input" });
+    }
+
+    user.findUserByUsername(userName, (err, responseData) => {
+        if (err) {
+            console.log(`Error finding user: ${err}`);
+            res.status(400).json({ error: err });
+            return next(err);
+        }
+        else if (!responseData) {
+            console.log('Missing `done()` argument');
+            res.status(400).json({ error: "Error finding user record" });
+            return next({ error: "Error finding user record" });
+        }
+        else if (responseData.toString().toLowerCase().includes('error' || 'exists' || 'already')) {
+            console.log(`Error in findUserByUsername response data: ${JSON.stringify(responseData)}`);
+            res.status(400).json({ error: "User does not exists with provided username. Please try again with correct username." });
+        }
+        else if (Object.entries.length == 0) {
+            res.status(400).json({ error: "Error finding user" });
+        }
+        else {
+            console.log(`Find By Username Response Data: ${responseData}`);
+            res.status(200).json({ _id: responseData.id, username: responseData.username });
         }
     });
 });
