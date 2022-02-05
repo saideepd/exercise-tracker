4dfd90d8069c986c6423096ceb864e031dfd2134
Saideep Dicholkar <dicholkarsaideep@gmail.com>
2022-02-05 22:18:31 +0530

    Added API endpoints for Add Exercise & Retrieve Exercise Logs for user

--- server/functions/exercise.js
+++ server/functions/exercise.js
@@ -1,16 +1,131 @@
 const mongoose = require('mongoose');
-const User = require('./user').UserModel;
+const user = require('./user');
 
 
 const exerciseSchema = mongoose.Schema({
-    username: { type: String, required: true },
-    description: { type: String, required: true },
-    duration: { type: Number, required: true },
-    date: { type: Date, required: true, default: Date.now },
-    _id: { type: mongoose.Schema.Types.ObjectId, ref: User }
+    username: {
+        type: String,
+        required: true
+    },
+    description: {
+        type: String,
+        required: true
+    },
+    duration: {
+        type: Number,
+        required: true
+    },
+    date: {
+        type: Date,
+        required: true,
+        default: Date.now
+    },
+    userId: {
+        type: mongoose.Schema.Types.ObjectId,
+        ref: user.UserModel
+    }
 });
 
 
 const Exercise = mongoose.model('Exercise', exerciseSchema);
 
+const addExercise = (input, done) => {
+    if (input === undefined || input === null) {
+        console.log(`Invalid input for Exercise`);
+        done(null, {
+            error: `Invalid input for Exercise`
+        });
+        return {
+            error: `Invalid input for Exercise`
+        };
+    }
+
+    console.log(`Input to addExercise: ${JSON.stringify(input)}, id: ${input._id}`);
+
+    // Find the user's username by calling findUserById() from User function
+    let findUser = user.findUserById(input._id, (req, res, next) => {
+        console.log(`findUser in Exercise: ${res}`);
+
+        // Form an exercise object to pss to create()
+        // Only using username from FindUserById response
+        // rest all the data is used from user input
+        let exerciseObject = Exercise({
+            username: res.username,
+            description: input.description,
+            duration: input.duration,
+            date: input.date,
+            userId: input._id
+        });
+
+        console.log(`ExerciseObject: ${exerciseObject}`);
+
+        // Create the exercise record in DB
+        Exercise.create(exerciseObject, (createExerciseError, createdExercise) => {
+            // In case of Error, return error message
+            if (createExerciseError) {
+                console.log(`Error creating exercise record: ${createExerciseError}`);
+                done(null, {
+                    error: "Error adding exercise"
+                });
+                return {
+                    error: "Error adding exercise"
+                };
+            }
+            // Else return response
+            console.log(`Exercise logged successfully: ${createdExercise}`);
+            done(null, createdExercise);
+            return createdExercise;
+        });
+    });
+}
+
+const getExerciseLog = (input, done) => {
+    let logInput = JSON.stringify(input);
+    console.log(`Exercise Log Input: ${logInput}`);
+
+    let { userId, from, to, limit } = JSON.parse(logInput);
+    console.log(`Exercise Log Input Values: ${userId}, ${from}, ${to}, ${limit}`);
+
+    // Find Exercise logs for user by querying using UserId
+    Exercise.find({
+        userId: { $eq: userId }
+    }, (logFindError, logFound) => {
+        if (logFindError) {
+            console.log(`Error finding exercise logs for user: ${logFindError}`);
+            done(null, {
+                error: "Error finding exercise logs for user"
+            });
+            return {
+                error: "Error finding exercise logs for user"
+            };
+        }
+
+        console.log(`Successfully found exercise logs for user: ${logFound}`);
+
+        let log = [];
+        logFound.map((item) => {
+            log.push({
+                description: item.description,
+                duration: item.duration,
+                date: item.date.toDateString()
+            });
+        });
+
+        // console.log(`ResponseLog: ${JSON.stringify(log)}`);
+
+        let logResponseObject = {
+            _id: logFound[0].userId,
+            username: logFound[0].username,
+            from: new Date(from).toDateString(),
+            to: new Date(to).toDateString(),
+            count: logFound.length,
+            log
+        };
+        done(null, logResponseObject);
+        return logResponseObject;
+    });
+}
+
 exports.ExerciseModel = Exercise;
+exports.addExercise = addExercise;
+exports.getExerciseLog = getExerciseLog;
--- server/index.js
+++ server/index.js
@@ -6,6 +6,7 @@ const mongoose = require('mongoose');
 const serverless = require('serverless-http');
 const router = express.Router();
 const user = require('./functions/user');
+const exercise = require('./functions/exercise');
 
 const PORT = process.env.PORT || 3001;
 
@@ -14,10 +15,14 @@ const app = express();
 app.listen();
 
 // Enable CORS
-app.use(cors({ optionsSuccessStatus: 200 }));
+app.use(cors({
+    optionsSuccessStatus: 200
+}));
 
 // Adding body-parser middleware
-app.use(bodyParser.urlencoded({ extended: false }));
+app.use(bodyParser.urlencoded({
+    extended: false
+}));
 
 // Hide X-powered-by header ;)
 app.use(function (req, res, next) {
@@ -26,21 +31,25 @@ app.use(function (req, res, next) {
 });
 
 // Connect to Mongo DB
-let connection = mongoose.connect
-    (process.env.MONGO_URI, {
-        useNewUrlParser: true,
-        useUnifiedTopology: true
-    });
+let connection = mongoose.connect(process.env.MONGO_URI, {
+    useNewUrlParser: true,
+    useUnifiedTopology: true
+});
 
 // Default API Endpoint
 router.get("/api", (req, res) => {
-    res.json({ message: "Hello from Exercise Tracker!" });
+    res.json({
+        message: "Hello from Exercise Tracker!"
+    });
 });
 
 // Test Hello Endpoint
 router.get("/api/hello", (req, res) => {
-    res.json({ message: "Hello, World!" });
+    res.json({
+        message: "Hello, World!"
+    });
 });
+
 
 // Create User API Endpoint
 router.post("/api/users", (req, res, next) => {
@@ -50,34 +59,48 @@ router.post("/api/users", (req, res, next) => {
     // Error handling for invalid input
     if (!username || username === undefined || typeof username === undefined || username === '') {
         console.log(`Invalid or blank username input`);
-        res.json({ error: "Invalid username input" });
-        return next({ error: "Invalid username input" });
+        res.json({
+            error: "Invalid username input"
+        });
+        return next({
+            error: "Invalid username input"
+        });
     }
 
     user.createUser(username, (err, responseData) => {
         if (err) {
             console.log(`Error creating user: ${err}`);
-            res.status(400).json({ error: err });
+            res.status(400).json({
+                error: err
+            });
             return next(err);
-        }
-        else if (!responseData) {
+        } else if (!responseData) {
             console.log('Missing `done()` argument');
-            res.status(500).json({ error: "Error creating record" });
-            return next({ error: "Error creating record" });
-        }
-        else if (responseData.toString().toLowerCase().includes('error' || 'exists' || 'already')) {
+            res.status(500).json({
+                error: "Error creating record"
+            });
+            return next({
+                error: "Error creating record"
+            });
+        } else if (responseData.toString().toLowerCase().includes('error' || 'exists' || 'already')) {
             console.log(`Error in create user response data: ${JSON.stringify(responseData)}`);
-            res.status(422).json({ error: "Username already taken. Please try again with another username." });
-        }
-        else if (Object.entries.length == 0) {
-            res.status(422).json({ error: "Error creating user" });
-        }
-        else {
+            res.status(422).json({
+                error: "Username already taken. Please try again with another username."
+            });
+        } else if (Object.entries.length == 0) {
+            res.status(422).json({
+                error: "Error creating user"
+            });
+        } else {
             console.log(`Create User Response Data: ${responseData}`);
-            res.status(201).json({ _id: responseData.id, username: responseData.username });
+            res.status(201).json({
+                _id: responseData.id,
+                username: responseData.username
+            });
         }
     });
 });
+
 
 // Find User By Id API Endpoint
 router.get("/api/users/:_id", (req, res, next) => {
@@ -87,34 +110,48 @@ router.get("/api/users/:_id", (req, res, next) => {
     // Error handling for invalid input
     if (!userId || userId === undefined || typeof userId === undefined || userId === '') {
         console.log(`Invalid or blank userId input`);
-        res.json({ error: "Invalid userId input" });
-        return next({ error: "Invalid userId input" });
+        res.json({
+            error: "Invalid userId input"
+        });
+        return next({
+            error: "Invalid userId input"
+        });
     }
 
     user.findUserById(userId, (err, responseData) => {
         if (err) {
             console.log(`Error finding user: ${err}`);
-            res.status(400).json({ error: err });
+            res.status(400).json({
+                error: err
+            });
             return next(err);
-        }
-        else if (!responseData) {
+        } else if (!responseData) {
             console.log('Missing `done()` argument');
-            res.status(400).json({ error: "Error finding user record" });
-            return next({ error: "Error finding user record" });
-        }
-        else if (responseData.toString().toLowerCase().includes('error' || 'exists' || 'already')) {
+            res.status(400).json({
+                error: "Error finding user record"
+            });
+            return next({
+                error: "Error finding user record"
+            });
+        } else if (responseData.toString().toLowerCase().includes('error' || 'exists' || 'already')) {
             console.log(`Error in findUserById response data: ${JSON.stringify(responseData)}`);
-            res.status(400).json({ error: "User does not exists with provided id. Please try again with correct user id." });
-        }
-        else if (Object.entries.length == 0) {
-            res.status(400).json({ error: "Error finding user" });
-        }
-        else {
+            res.status(400).json({
+                error: "User does not exists with provided id. Please try again with correct user id."
+            });
+        } else if (Object.entries.length == 0) {
+            res.status(400).json({
+                error: "Error finding user"
+            });
+        } else {
             console.log(`Find User By Id Response Data: ${responseData}`);
-            res.status(200).json({ _id: responseData.id, username: responseData.username });
+            res.status(200).json({
+                _id: responseData.id,
+                username: responseData.username
+            });
         }
     });
 });
+
 
 // Find User By Username API Endpoint
 router.get("/api/username/:username", (req, res, next) => {
@@ -124,32 +161,132 @@ router.get("/api/username/:username", (req, res, next) => {
     // Error handling for invalid input
     if (!userName || userName === undefined || typeof userName === undefined || userName === '') {
         console.log(`Invalid or blank username input`);
-        res.json({ error: "Invalid username input" });
-        return next({ error: "Invalid username input" });
+        res.json({
+            error: "Invalid username input"
+        });
+        return next({
+            error: "Invalid username input"
+        });
     }
 
     user.findUserByUsername(userName, (err, responseData) => {
         if (err) {
             console.log(`Error finding user: ${err}`);
-            res.status(400).json({ error: err });
+            res.status(400).json({
+                error: err
+            });
             return next(err);
-        }
-        else if (!responseData) {
+        } else if (!responseData) {
             console.log('Missing `done()` argument');
-            res.status(400).json({ error: "Error finding user record" });
-            return next({ error: "Error finding user record" });
-        }
-        else if (responseData.toString().toLowerCase().includes('error' || 'exists' || 'already')) {
+            res.status(400).json({
+                error: "Error finding user record"
+            });
+            return next({
+                error: "Error finding user record"
+            });
+        } else if (responseData.toString().toLowerCase().includes('error' || 'exists' || 'already')) {
             console.log(`Error in findUserByUsername response data: ${JSON.stringify(responseData)}`);
-            res.status(400).json({ error: "User does not exists with provided username. Please try again with correct username." });
-        }
-        else if (Object.entries.length == 0) {
-            res.status(400).json({ error: "Error finding user" });
-        }
-        else {
+            res.status(400).json({
+                error: "User does not exists with provided username. Please try again with correct username."
+            });
+        } else if (Object.entries.length == 0) {
+            res.status(400).json({
+                error: "Error finding user"
+            });
+        } else {
             console.log(`Find By Username Response Data: ${responseData}`);
-            res.status(200).json({ _id: responseData.id, username: responseData.username });
+            res.status(200).json({
+                _id: responseData.id,
+                username: responseData.username
+            });
         }
+    });
+});
+
+// Add Exercise for User API Endpoint
+router.post("/api/users/:_id/exercise", (req, res, next) => {
+    let exerciseInput = {
+        _id: req.body._id,
+        description: req.body.description,
+        duration: req.body.duration,
+        date: req.body.date
+    };
+    console.log(`Input body for exercise: ${JSON.stringify(exerciseInput)}`);
+
+    // Error handling for invalid input
+    if (!exerciseInput || exerciseInput === undefined || typeof exerciseInput === undefined || exerciseInput === '') {
+        console.log(`Invalid or blank exercise input`);
+        res.json({
+            error: "Invalid exercise input"
+        });
+        return next({
+            error: "Invalid exercise input"
+        });
+    }
+
+    exercise.addExercise(exerciseInput, (err, responseData) => {
+        if (err) {
+            console.log(`Error adding exercise: ${err}`);
+            res.status(400).json({
+                error: err
+            });
+            return next(err);
+        } else if (!responseData) {
+            console.log('Missing `done()` argument');
+            res.status(500).json({
+                error: "Error creating exericse record"
+            });
+            return next({
+                error: "Error creating exericse record"
+            });
+        } else if (JSON.stringify(responseData).toLowerCase().includes('error' || 'exists' || 'already')) {
+            console.log(`Error in add exericse response data: ${JSON.stringify(responseData)}`);
+            res.status(422).json({
+                error: "Error in add exericse response data"
+            });
+        } else if (Object.entries.length == 0) {
+            res.status(422).json({
+                error: "Error adding exercise"
+            });
+        } else {
+            console.log(`Add Exercise Response Data: ${responseData}`);
+            res.status(201).json({
+                username: responseData.username,
+                description: responseData.description,
+                duration: responseData.duration,
+                date: responseData.date,
+                _id: responseData.userId
+            });
+        }
+
+    });
+});
+
+
+// Get Exercise Logs for User API Endpoint
+router.get("/api/users/:_id/logs", (req, res, next) => {
+    // Get User Id from request Parameters
+    let userId = req.params._id;
+    // Get query opions from request query
+    let { from, to, limit } = req.query;
+
+    console.log(`Logs input: ${userId}, ${from}, ${to}, ${limit}`);
+
+    // Form exerciseLogsObject to send to server for retrieving data
+    let exerciseLogsObject = { userId: userId, from: from, to: to, limit: limit };
+
+    // Get the Exercise Logs for a user
+    let getLogs = exercise.getExerciseLog(exerciseLogsObject, (err, responseData) => {
+        if (err) {
+            console.log(`Error response: ${err}`);
+            res.status(400).json({
+                error: "Error getting exercise logs for user"
+            });
+            done(null, { error: "Error getting exercise logs for user" });
+            return { error: "Error getting exercise logs for user" };
+        }
+        console.log(`ExerciseLog responseData: ${JSON.stringify(responseData)}`);
+        res.status(200).json(responseData);
     });
 });
 
