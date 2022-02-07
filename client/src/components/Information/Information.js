79b7bc960236a73780d74052f88b1d210402f309
Saideep Dicholkar <dicholkarsaideep@gmail.com>
2022-02-08 00:34:46 +0530

    Updated Information component & some styling

--- client/src/components/Information/Information.js
+++ client/src/components/Information/Information.js
@@ -7,6 +7,12 @@ const Information = () => {
             <p>
                 <b>GET user's exercise log:</b>&nbsp;&nbsp;&nbsp;<span className="endpoint">GET /api/users/:_id/logs?[from][&to][&limit]</span>
             </p>
+            <p>
+                <b>[ ]</b> = <span>optional</span>
+            </p>
+            <p>
+                <b>from, to</b> = <span>dates (yyyy-mm-dd); </span><b>limit</b> = <span>number</span>
+            </p>
         </div>
     )
 }
