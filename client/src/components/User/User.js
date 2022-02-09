e4830fd681380e2ec20e9443a227ea7f40f0541c
Saideep Dicholkar <dicholkarsaideep@gmail.com>
2022-02-09 11:22:15 +0530

    Added basic form, Alerts, & form submit, change events

--- client/src/components/User/User.js
+++ client/src/components/User/User.js
@@ -1,6 +1,6 @@
-import React from 'react'
+import React, { useState, useReducer } from 'react'
 import './User.css';
-import { Button, Paper, TextField } from '@mui/material';
+import { Alert, Button, debounce, Paper, TextField } from '@mui/material';
 import { styled } from "@mui/material/styles";
 
 const CssTextField = styled(TextField)({
@@ -31,33 +31,82 @@ const ColorButton = styled(Button)({
   }
 });
 
+const formReducer = (state, event) => {
+  return {
+    ...state,
+    [event.name]: event.value
+  };
+}
+
 const User = () => {
+  const [formData, setFormData] = useReducer(formReducer, {})
+  const [submitting, setSubmitting] = useState(false);
+  const handleSubmit = (event) => {
+    event.preventDefault();
+    setTimeout(() => {
+      setSubmitting(true);
+
+      console.log("You've submitted the form");
+      alert("You've submitted the form");
+      // setSubmitting(false);
+    }, 3000);
+  }
+
+  const handleChange = (event) => {
+    setFormData({
+      name: event.target.name,
+      value: event.target.value
+    });
+    console.log(`name: ${event.target.name}, value: ${event.target.value}`)
+  }
+
+  const handleText = debounce((event) => {
+    handleChange(event);
+  }, 500);
+
   return (
     <div className="user-component">
-      <Paper
-        elevation={3}
-        className="user-paper"
-        style={{ backgroundColor: '#fff9c4' }}
-      >
-        <h3>Create a New User</h3>
-        <p className="endpoint user-endpoint">POST /api/users</p>
-        <CssTextField
-          required
-          id="outlined-required"
-          label="Username"
-          name="username"
-          placeholder="username"
-          className="user-input"
-          size="small"
-        />
-        <ColorButton
-          variant="contained"
-          id="create-user-btn"
-          color="primary"
+      <form onSubmit={handleSubmit}>
+        <Paper
+          elevation={3}
+          className="user-paper"
+          style={{ backgroundColor: '#fff9c4' }}
         >
-          Submit
-        </ColorButton>
-      </Paper>
+          <h3>Create a New User</h3>
+          <p className="endpoint user-endpoint">POST /api/users</p>
+          <CssTextField
+            required
+            id="outlined-required"
+            label="Username"
+            name="username"
+            placeholder="username"
+            className="user-input"
+            size="small"
+            onChange={(event) => handleText(event)}
+          />
+          <ColorButton
+            variant="contained"
+            id="create-user-btn"
+            color="primary"
+            type="submit"
+          >
+            Submit
+          </ColorButton>
+        </Paper>
+        {submitting &&
+          <div>
+            <br />
+            <Alert variant="filled" severity="success">
+              Submitted Successfully!
+            </Alert>
+            <ul>
+              {Object.entries(formData).map(([name, value]) => (
+                <li key={name+value}><strong>{name}</strong>:{value}</li>
+              ))}
+            </ul>
+          </div>
+        }
+      </form>
     </div>
   )
 }
