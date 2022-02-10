ddea300bb12f938d582287de783825757166b1d9
Saideep Dicholkar <dicholkarsaideep@gmail.com>
2022-02-10 22:32:41 +0530

    Added code to consume create user API and displaying response from React client side

--- client/src/components/User/User.js
+++ client/src/components/User/User.js
@@ -2,6 +2,9 @@ import React, { useState, useReducer } from 'react'
 import './User.css';
 import { Alert, Button, debounce, Paper, TextField } from '@mui/material';
 import { styled } from "@mui/material/styles";
+import axios from 'axios';
+
+const baseUrl = "http://localhost:8888";
 
 const CssTextField = styled(TextField)({
   '& label.Mui-focused': {
@@ -24,6 +27,7 @@ const CssTextField = styled(TextField)({
   },
 });
 
+
 const ColorButton = styled(Button)({
   backgroundColor: '#304fff',
   '&:hover': {
@@ -38,17 +42,73 @@ const formReducer = (state, event) => {
   };
 }
 
+let isSubmitted = false;
+
 const User = () => {
   const [formData, setFormData] = useReducer(formReducer, {})
   const [submitting, setSubmitting] = useState(false);
+  const [post, setPost] = useState(null);
+
+  // Call the Create User API with POST request
+  const createUser = async () => {
+    console.log('called create user response');
+    await axios.post(`${baseUrl}/api/users`, {
+      username: formData.username
+    }
+    )
+      .then((response) => {
+        setPost(response.data)
+      })
+      .catch((error) => {
+        console.log(`Error: ${error}, formData:${JSON.stringify(formData)}, post: ${JSON.stringify(post)}, type: ${typeof post}`)
+      })
+  };
+
+
+  // Get the user by username
+  const getUserByUsername = async () => {
+    console.log(`Inside getByUsername: ${formData.username}`);
+    await axios.get(`${baseUrl}/api/username/${formData.username}`)
+      .then((response) => {
+        setPost(response.data)
+      })
+      .catch((error) => {
+        console.log(`Error in getByUsername: ${error}, formData: ${formData.username}`);
+      })
+  };
+
+
   const handleSubmit = (event) => {
     event.preventDefault();
+    // Setting Submit to true in 1s
     setTimeout(() => {
+      console.log('called handleSubmit to true');
       setSubmitting(true);
+      isSubmitted = true;
 
       console.log("You've submitted the form");
-      alert("You've submitted the form");
-      // setSubmitting(false);
+      // alert("You've submitted the form");
+    }, 1000);
+
+    // Setting Submit to false in next 3s
+    setTimeout(() => {
+      // Retry pattern
+      // TODO: Instead of POST request for retry,
+      // need to use getByUserName to fetch submitted user details
+      // if (post === null && count < 1) {
+      //   console.log(`Retry atttempt count: ${count}`);
+      //   // handleSubmit(event);
+      //   getUserByUsername();
+      //   console.log(`createUserResponse getByUsername: ${JSON.stringify(post)}`);
+      //   count++;
+      // }
+
+      let createUserResponse = createUser();
+      console.log(`createUserResponse: ${JSON.stringify(post)}`);
+      console.log('called createUser()');
+
+      setSubmitting(false);
+      console.log('Called handleSubmit to false');
     }, 3000);
   }
 
@@ -83,12 +143,14 @@ const User = () => {
             className="user-input"
             size="small"
             onChange={(event) => handleText(event)}
+            disabled={submitting}
           />
           <ColorButton
             variant="contained"
             id="create-user-btn"
             color="primary"
             type="submit"
+            disabled={submitting}
           >
             Submit
           </ColorButton>
@@ -99,10 +161,17 @@ const User = () => {
             <Alert variant="filled" severity="success">
               Submitted Successfully!
             </Alert>
+          </div>
+        }
+        {isSubmitted &&
+          <div>
             <ul>
-              {Object.entries(formData).map(([name, value]) => (
-                <li key={name+value}><strong>{name}</strong>:{value}</li>
-              ))}
+              {post !== null && (
+                <li key={post._id}><strong>_id:</strong> {post._id}</li>
+              )}
+              {post !== null && (
+                <li key={post.username}><strong>username:</strong> {post.username}</li>
+              )}
             </ul>
           </div>
         }
