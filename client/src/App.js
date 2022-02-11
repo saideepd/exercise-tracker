4fb8aaa8d15b8c560075376e251ec94550f2e68c
Saideep Dicholkar <dicholkarsaideep@gmail.com>
2022-02-12 02:43:23 +0530

    Exercise Component functionality completed Added code to call Create Exercise Log API endpoint Added logic to auto-fill User Id from User component to Exercise Component Added Exercise variables to main root App level Some other UI fixes

--- client/src/App.js
+++ client/src/App.js
@@ -6,6 +6,7 @@ import User from './components/User/User';
 import Exercise from './components/ExerciseLog/ExerciseLog';
 import { Container } from '@mui/material';
 
+const baseUrl = "http://localhost:8888";
 
 function App() {
 
@@ -17,12 +18,23 @@ function App() {
       [event.name]: event.value
     };
   }
+  // const formReducerExercise = (state, event) => {
+  //   return {
+  //     ...state,
+  //     [event.name]: event.value
+  //   };
+  // }
 
   // User variables
   const [formData, setFormData] = useReducer(formReducer, {})
   const [submitting, setSubmitting] = useState(false);
   const [post, setPost] = useState(null);
 
+  // Exercise variables
+  const [userId, setUserId] = useState(null);
+  const [exerciseSubmit, setExerciseSubmit] = useState(false);
+  const [exerciseLog, setExerciseLog] = useReducer(formReducer, {});
+  const [log, setLog] = useState({});
 
   return (
     <div className="App">
@@ -38,9 +50,19 @@ function App() {
             setSubmitting={setSubmitting}
             post={post}
             setPost={setPost}
+            baseUrl={baseUrl}
           />
           <Exercise 
             formData={formData}
+            baseUrl={baseUrl}
+            userId={userId}
+            setUserId={setUserId}
+            exerciseSubmit={exerciseSubmit}
+            setExerciseSubmit={setExerciseSubmit}
+            exerciseLog={exerciseLog}
+            setExerciseLog={setExerciseLog}
+            log={log}
+            setLog={setLog}
           />
         </div>
         <Information />
--- client/src/components/ExerciseLog/ExerciseLog.js
+++ client/src/components/ExerciseLog/ExerciseLog.js
@@ -1,8 +1,9 @@
 import React from 'react'
 import './ExerciseLog.css';
-import { Button, Paper, TextField } from '@mui/material';
+import { Alert, Button, debounce, Paper, TextField } from '@mui/material';
 import { styled } from "@mui/material/styles";
-import { currentUsername, isSubmitted } from '../User/User';
+import { isSubmitted } from '../User/User';
+import axios from 'axios';
 
 const CssTextField = styled(TextField)({
   '& label.Mui-focused': {
@@ -32,64 +33,150 @@ const ColorButton = styled(Button)({
   }
 });
 
-const ExerciseLog = ({formData}) => {
+// let exerciseLogArray = [];
+
+const ExerciseLog = ({ formData, baseUrl, userId, setUserId, exerciseSubmit, setExerciseSubmit, exerciseLog, setExerciseLog, log, setLog }) => {
+
+  // API to Create Exercise Log
+  const createExerciseLog = async () => {
+    console.log('Called createExerciseLog()');
+    console.log(`axios exerciseLog: ${JSON.stringify(exerciseLog)}`)
+    // Add User id if payload object is missing _id
+    if (!exerciseLog.hasOwnProperty('_id'))
+      exerciseLog._id = formData.userId;
+
+    await axios.post(`${baseUrl}/api/users/${formData.userId}/exercise`,
+      exerciseLog
+    )
+      .then((response) => {
+        setLog(response.data)
+        console.log(`Create Exercise Log Response Data: ${JSON.stringify(response.data)}`)
+      })
+      .catch((error) => {
+        console.log(`Error in Create Exercise Log: ${error}, log: ${JSON.stringify(log)}`);
+      })
+  };
+
+  const handleSubmit = (event) => {
+    event.preventDefault();
+    // Setting submit to true in 0.1s
+    setTimeout(() => {
+      console.log('Called HandleSubmit to true')
+      setExerciseSubmit(true);
+      // isSubmitted = true;
+      // Adding extra user property explicitly
+      // exerciseLog._id = formData.userId;
+      console.log(`exerciseLog: ${JSON.stringify(exerciseLog)}`)
+
+      console.log("You've submitted Exercise form");
+    }, 100);
+
+    // Setting submit to false in next 3s
+    setTimeout(() => {
+
+      // Call create exercise log method
+      let createExerciseLogResponse = createExerciseLog();
+
+      console.log('called createExerciseLog()');
+
+      // Set submitted to false after 3s
+      setExerciseSubmit(false);
+      // setExerciseSubmit(false);
+      console.log('Called HandleSubmit to false');
+    }, 3000);
+  };
+
+  const handleChange = (event) => {
+    setExerciseLog({
+      name: event.target.name,
+      value: event.target.value
+    });
+    // let [key, value] = [event.target.name, event.target.value];
+    // exerciseLogArray.push(...exerciseLogArray, {key: value})
+    console.log(`Exercise name: ${event.target.name}, value: ${event.target.value}`);
+    // console.log(`Exercise key: ${key}, value: ${value}`);
+  }
+
+  const handleUserExercise = debounce((event) => {
+    console.log("Inside handleUserExercise")
+    handleChange(event);
+  }, 500);
+
+
   return (
     <div className="exercise-component">
-      <Paper
-        elevation={3}
-        className="exercise-paper"
-        style={{ backgroundColor: '#fff9c4', paddingTop: '0.5em' }}
-      >
-        <h3>Add exercises</h3>
-        <p className="endpoint exercise-endpoint">POST /api/users/:_id/exercises</p>
-        <CssTextField
-          required
-          id="outlined-required"
-          label="User Id"
-          name="UserId"
-          placeholder=":_id"
-          className="user-input"
-          size="small"
-          margin="dense"
-        />
-        <CssTextField
-          required
-          id="outlined-required"
-          label="Description"
-          name="Description"
-          placeholder="Description"
-          className="user-input"
-          size="small"
-          margin="dense"
-        />
-        <CssTextField
-          required
-          id="outlined-required"
-          label="Duration"
-          name="Duration"
-          placeholder="Duration (mins.)"
-          className="user-input"
-          size="small"
-          margin="dense"
-        />
-        <CssTextField
-          required
-          id="outlined-required"
-          label="Date"
-          name="Date"
-          placeholder="Date (yyyy-mm-dd)"
-          className="user-input"
-          size="small"
-          margin="dense"
-        />
-        <ColorButton
-          variant="contained"
-          id="create-exerciselog-btn"
-          color="primary"
+      <form onSubmit={handleSubmit}>
+        <Paper
+          elevation={3}
+          className="exercise-paper"
+          style={{ backgroundColor: '#fff9c4', paddingTop: '0.5em' }}
         >
-          Submit
-        </ColorButton>
-        {isSubmitted && 
+          <h3>Add exercises</h3>
+          <p className="endpoint exercise-endpoint">POST /api/users/:_id/exercises</p>
+          <CssTextField
+            required
+            id="outlined-required"
+            label="User Id"
+            name="_id"
+            placeholder=":_id"
+            className="user-input"
+            size="small"
+            margin="dense"
+            title="User Id will be auto-filled"
+            value={!isSubmitted ? "" : formData.userId}
+            onChange={handleUserExercise}
+            disabled={exerciseSubmit}
+          />
+          <CssTextField
+            required
+            id="outlined-required"
+            label="Description"
+            name="description"
+            placeholder="Description"
+            className="user-input"
+            size="small"
+            margin="dense"
+            title="Describe the exercise"
+            onChange={handleUserExercise}
+            disabled={exerciseSubmit}
+          />
+          <CssTextField
+            required
+            id="outlined-required"
+            label="Duration"
+            name="duration"
+            placeholder="Duration (mins.)"
+            className="user-input"
+            size="small"
+            margin="dense"
+            title="How long did you exercised?"
+            onChange={handleUserExercise}
+            disabled={exerciseSubmit}
+          />
+          <CssTextField
+            required
+            id="outlined-required"
+            label="Date"
+            name="date"
+            placeholder="Date (yyyy-mm-dd)"
+            className="user-input"
+            size="small"
+            margin="dense"
+            title="Enter the date of exercise in yyyy-mm-dd format"
+            onChange={handleUserExercise}
+            disabled={exerciseSubmit}
+          />
+          <ColorButton
+            variant="contained"
+            id="create-exerciselog-btn"
+            color="primary"
+            type="submit"
+            disabled={exerciseSubmit}
+          >
+            Submit
+          </ColorButton>
+        </Paper>
+        {/* {isSubmitted &&
           console.log(`Exercise currentUsername: ${JSON.stringify(currentUsername)}, formData: ${JSON.stringify(formData)}`)
           // <div>
           //   <ul>
@@ -101,8 +188,25 @@ const ExerciseLog = ({formData}) => {
           //     )}
           //   </ul>
           // </div>
+        } */}
+        {exerciseSubmit &&
+          <div>
+            <br />
+            <Alert variant="filled" severity="success">
+              Exercise Logged Successfully!
+            </Alert>
+          </div>
         }
-      </Paper>
+        {isSubmitted &&
+          <div>
+            <ul>
+              {log !== null && Object.entries(log).map(([key, value]) => (
+                <li key={key + value}><strong>{key}:</strong> {value}</li>
+              ))}
+            </ul>
+          </div>
+        }
+      </form>
     </div>
   )
 }
