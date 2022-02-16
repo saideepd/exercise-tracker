0d7c6ee3cdc6a5c85c28b11929ec7220fc123cdc
Saideep Dicholkar <dicholkarsaideep@gmail.com>
2022-02-17 01:21:58 +0530

    Using App Icon as Header text

--- client/src/components/Header/Header.js
+++ client/src/components/Header/Header.js
@@ -4,7 +4,7 @@ import './Header.css';
 const Header = () => {
     return (
         <div className="header">
-            <h1>Fit Tracker</h1>
+            <h1>Fit <img src="apple-touch-icon.png" alt="T" style={{ width: "30px", height: "auto", verticalAlign: "baseline" }} />racker</h1>
         </div>
     )
 }
